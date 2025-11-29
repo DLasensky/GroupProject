75f580a2c91d7fd8836f52922faebd715f2d0b1d
Dalyourprogrammer <DHOLM@Rollins.edu>
2025-11-28 19:55:28 -0500

    Addition of Add and Remove panel functions
    I added Add and Remove buttons that include separate panels to allow input for the menu item fields for adding items and selection of items from list for removal.
    
    I am having trouble on my end running the code due to package problems, forcing me to run the code on my other workspace. I couldn't fix this issue on my end so let me know if you get the same issue and if you find any way to resolve it. I'm available all this weekend if you guys want to zoom call.

--- src/MenuFrame.java
+++ src/MenuFrame.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.awt.event.*;
 import java.util.List;
 
-/// cafe window with a simple list and filters
+/*
+ *  cafe window with a simple list and filters
+ */
 
 public class MenuFrame extends JFrame {
 
@@ -11,6 +13,7 @@ public class MenuFrame extends JFrame {
     private DefaultListModel<String> listModel;
     private JList<String> list;
 
+    //set up main panel
     public MenuFrame(LoginFrame loginRef) {
     	
         this.loginRef = loginRef;
@@ -26,13 +29,14 @@ public class MenuFrame extends JFrame {
 
         menuManager = new MenuManager();
         loadSampleData();
-
+        
+        //add scrollable table to main panel
         listModel = new DefaultListModel<String>();
         list = new JList<String>(listModel);
         JScrollPane scroll = new JScrollPane(list);
         scroll.setBounds(30, 50, 430, 200);
         add(scroll);
-
+        //add buttons for main panel
         JButton allBtn = new JButton("All");
         allBtn.setBounds(30, 270, 90, 30);
         add(allBtn);
@@ -52,7 +56,16 @@ public class MenuFrame extends JFrame {
         JButton logoutBtn = new JButton("Logout");
         logoutBtn.setBounds(200, 310, 90, 25);
         add(logoutBtn);
-
+        
+        JButton addBtn = new JButton("Add");
+        addBtn.setBounds(100,310,90,25);
+        add(addBtn);
+        
+        JButton removeBtn = new JButton("Remove");
+        removeBtn.setBounds(300,310,90,25);
+        add(removeBtn);
+        
+        //add Action Listeners for buttons in main panel
         allBtn.addActionListener(new ActionListener() {
         	
             public void actionPerformed(ActionEvent e) {
@@ -100,7 +113,10 @@ public class MenuFrame extends JFrame {
                 
             }
         });
-
+        
+  //call Add and Remove dialog panels when buttons are pressed from main panel
+        addBtn.addActionListener(e-> openAddItemDialog());
+        removeBtn.addActionListener(e-> openRemoveDialog());
         showItems(menuManager.getAllItems());
 
         setLocationRelativeTo(null);
@@ -131,18 +147,9 @@ public class MenuFrame extends JFrame {
         menuManager.addItem(new Snack("Pretzel Bites", 2.49, 170, "Soft pretzel bites with cheese dip", "SnackCo"));
         menuManager.addItem(new Snack("Dark Chocolate", 1.29, 160, "Rich dark chocolate square", "ChocoLux"));
         menuManager.addItem(new Snack("Edamame", 3.49, 190, "Steamed soybeans with sea salt", "AsianBites"));
-
-
-        Java restaurant menu management system tutorial
-
-
-
-
-
-
-        
     }
 
+    //allows the menu to be displayed
     private void showItems(List<MenuItem> items) {
     	
         listModel.clear();
@@ -150,5 +157,148 @@ public class MenuFrame extends JFrame {
         	
             listModel.addElement(mi.displayInfo());
             
-        }
+        }}
+    
+    //Add panel from add button on main
+    private void openAddItemDialog() {
+    	JDialog dialog = new JDialog(this, "Add Menu Item", true);
+    	dialog.setSize(300,350);
+    	dialog.setLayout(null);
+    	dialog.setLocationRelativeTo(this);
+    	
+    	//adds labels, fields, etc.
+    	JLabel nameLabel = new JLabel("Name:");
+    	nameLabel.setBounds(20,20,80,25);
+    	dialog.add(nameLabel);
+    	
+    	JTextField nameField = new JTextField();
+    	nameField.setBounds(100,20,150,25);
+    	dialog.add(nameField);
+    	
+    	JLabel priceLabel = new JLabel("Price:");
+    	priceLabel.setBounds(20,60,80,25);
+    	dialog.add(priceLabel);
+    	
+    	JTextField priceField = new JTextField();
+    	priceField.setBounds(100,60,150,25);
+    	dialog.add(priceField);
+    	
+    	JLabel calLabel = new JLabel("Calories:");
+    	calLabel.setBounds(20,100,80,25);
+    	dialog.add(calLabel);
+    	
+    	JTextField calField = new JTextField();
+    	calField.setBounds(100,100,150,25);
+    	dialog.add(calField);
+    	
+    	JLabel descLabel = new JLabel("Description:");
+    	descLabel.setBounds(20,140,80,25);
+    	dialog.add(descLabel);
+    	
+    	JTextField descField = new JTextField();
+    	descField.setBounds(100,140,150,25);
+    	dialog.add(descField);
+    	
+    	JLabel catLabel = new JLabel("Category:");
+    	catLabel.setBounds(20,220,150,25);
+    	dialog.add(catLabel);
+    	
+    	//scrollable box for category list
+    	JComboBox<String> categoryBox = new JComboBox<>(new String[] {"Drink","Snack","Meal"});
+    	categoryBox.setBounds(100,220,150,25);
+    	dialog.add(categoryBox);
+    	
+    	JLabel extraLabel = new JLabel("Size/Type/Brand:");
+    	extraLabel.setBounds(20,180,150,25);
+    	dialog.add(extraLabel);
+    	
+    	JTextField extraField = new JTextField();
+    	extraField.setBounds(125,180,125,25);
+    	dialog.add(extraField);
+    	
+    	JButton addBtn = newJButton("Add");
+    	addBtn.setBounds(100,250,100,30);
+    	dialog.add(addBtn);
+    	
+    	//Action Listener to adjust extra label based on set category
+    	categoryBox.addActionListener(e->{
+    		String selected = (String) categoryBox.getSelectedItem();
+    		if(selected.equals("Drink")) {
+    			extraLabel.setText("Size");
+    		}
+    		else if(selected.equals("Meal")) {
+    			extraLabel.setText("Type");
+    		}
+    		else {
+    			extraLabel.setText("Brand");
+    	}});
+    	
+    	//Action Listener to add item with all variables considered
+    	addBtn.addActionListener(e->{
+    		try {
+    			String name = nameField.getText().trim();
+    			double price = Double.parseDouble(priceField.getText().trim());
+    			String desc = descField.getText().trim();
+    			int cals = Integer.parseInt(calField.getText().trim());
+    			String extra = extraField.getText().trim();
+    			String cat = (String) categoryBox.getSelectedItem();
+    			
+    			MenuItem = newItem;
+    			
+    			//adjust category based on selection of category box
+    			switch(cat) {
+    			case "Drink":
+    				newItem = new Drink(name,price,cals,desc,extra);
+    				break;
+    			case "Meal":
+    				newItem = new Meal(name,price,cals,desc,extra);
+    				break;
+    			default:
+    				newItem = new Snack(name,price,cals,desc,extra);
+    				break;
+    			
+    			
+    			menuManager.addItem(newItem);
+    			listModel.addElement(new.dislayInfo());
+    			dialog.dispose();
+    	}catch(Exception ex) {
+    		JOptionPane.showMessageDialog(dialog,"Invalid input!");
+    	}}});
+    	dialog.setVisible(true);
+    	}
+    	
+    //panel for remove button on main
+    private void openRemoveDialog() {
+    	JDialog dialog = new JDialog(this,"Remove Item",true);
+    	dialog.setSize(300,250);
+    	dialog.setLayout(null);
+    	dialog.setLocationRelaticeTo(this);
+    	
+    	JLabel = new JLabel("Select Item:");
+    	label.setBounds(20,20,100,25);
+    	dialog.add(label);
+    	
+    	//scrollable box of menu item
+    	JComboBox<String> dropdown = new JComboBox<>();
+    	for(int i=0;i<listModel.size();i++) {
+    		dropdown.addItem(listModel.get(i));
+    		}
+    	dropdown.setBounds(100,20,160,25);
+    	dialog.add(dropdown);
+    	
+    	JButton removeBtn = new JButton("Remove");
+    	removeBtn.setBounds(90,60,100,30);
+    	dialog.add(removeBtn);
+    	
+    	//Action Listener to remove selected item when remove button is pressed
+    	removeBtn.addActionListener(e->{
+    		String selected = (String) dropdown.getSelectedItem();
+    		if(selected != null) {
+    			listModel.removeElement(selected);
+    			menuManager.getAllItem().removeIf(i->i.displayInfo().equals(selected));
+    		}
+    		dialog.dispose();
+    	});
+    	dialog.setVisible(true);
+    	}
     }
